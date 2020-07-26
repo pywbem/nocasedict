278381c6fdce9ff29b4ff44979455ddfc1672815
Andreas Maier <andreas.r.maier@gmx.de>
2020-07-26 11:26:24 +0200

    Switched to using sphinx_rtd_theme
    This addresses some issues with the Python 3 theme (copyright,
    suggesting this is a PSF-provided package).
    
    Signed-off-by: Andreas Maier <andreas.r.maier@gmx.de>

--- docs/conf.py
+++ docs/conf.py
@@ -70,7 +70,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',   # disabed, raises anexception
     'sphinx.ext.ifconfig',
-    'python_docs_theme',
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -214,15 +214,17 @@ viewcode_import = True
 
 # The theme to use for HTML and HTML Help pages.
 # See https://www.sphinx-doc.org/en/stable/theming.html for built-in themes.
-html_theme = 'python_docs_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.
 # See https://www.sphinx-doc.org/en/stable/theming.html for the options
 # available for built-in themes.
+# For options of the 'sphinx_rtd_theme', see
+# https://sphinx-rtd-theme.readthedocs.io/en/latest/configuring.html
 html_theme_options = {
-    'collapsiblesidebar': True,
-    'issues_url': 'https://github.com/pywbem/nocasedict/issues',
+    'style_external_links': False,
+    'collapse_navigation': False,
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
